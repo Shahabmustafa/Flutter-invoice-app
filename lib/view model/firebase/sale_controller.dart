a5391045308213e2515f4fe5b81febd4c2855c3f
Shahabmustafa <shahabmustafa57@gmail.com>
2024-01-03 13:09:39 +0500

    add sale

--- lib/view model/firebase/sale_controller.dart
+++ lib/view model/firebase/sale_controller.dart
@@ -1,7 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+
+import '../../res/app_api/app_api_service.dart';
 
 class SaleController extends GetxController{
 
+
+  Future<List<String>> itemsName() async {
+    List<String> dropdownItems = [];
+    try {
+      QuerySnapshot querySnapshot = await AppApiService.item.get();
+      if (querySnapshot.docs.isNotEmpty) {
+        dropdownItems = querySnapshot.docs.map((doc) => doc['itemName'].toString()).toList();
+      }
+    } catch (e) {
+      print('Error fetching data: $e');
+    }
+    return dropdownItems;
+  }
 
 
 }
--- lib/view/bottam navigator bar/sales/sales_screen.dart
+++ lib/view/bottam navigator bar/sales/sales_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_invoice_app/res/app_api/app_api_service.dart';
 import 'package:flutter_invoice_app/res/colors/app_colors.dart';
 import 'package:flutter_invoice_app/view%20model/firebase/item_controller.dart';
+import 'package:flutter_invoice_app/view%20model/firebase/sale_controller.dart';
 import 'package:get/get.dart';
 
 import '../../../view model/firebase/order_controller.dart';
@@ -16,9 +17,23 @@ class SalesPage extends StatefulWidget {
 }
 
 class _SalesPageState extends State<SalesPage> {
-  String? _category;
 
+  String? itemName;
+  List<String> itemDropdown = [];
+  final sale = Get.put(SaleController());
+
+  Future<void> itemNameSetState() async {
+    List<String> data = await sale.itemsName();
+    setState(() {
+      itemDropdown = data;
+    });
+  }
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    itemNameSetState();
+  }
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Scaffold(
@@ -26,15 +41,115 @@ class _SalesPageState extends State<SalesPage> {
         title: Text("Sale"),
         automaticallyImplyLeading: false,
       ),
-      body: StreamBuilder(
-        stream: AppApiService.item.snapshots(),
-        builder: (context,snapshot){
-          if(snapshot.hasData){
-            return Text("");
-          }else{
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: FormField<String>(
+              builder: (FormFieldState<String> state) {
+                return InputDecorator(
+                  decoration: InputDecoration(
+                    labelText: 'Item Name',
+                    errorText: state.errorText,
+                    border: InputBorder.none,
+                  ),
+                  isEmpty: itemName == null || itemName!.isEmpty,
+                  child: DropdownButtonFormField<String>(
+                    value: itemName,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                    items: itemDropdown.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        itemName = value;
+                      });
+                      state.didChange(value);
+                    },
+                  ),
+                );
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please select an option';
+                }
+                return null;
+              },
+            ),
+          ),
+          Container(
+            height: size.height * 0.25,
+            child: StreamBuilder(
+              stream: AppApiService.item.doc("${itemName}").snapshots(),
+              builder: (context,snapshot){
+                if(snapshot.hasData){
+                  Map<String,dynamic> data = snapshot.data!.data() as Map<String,dynamic>;
+                  return Container(
+                    height: size.height * 0.2,
+                    width: size.width,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade400,
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Sale",
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500
+                              ),
+                            ),
+                            SizedBox(
+                              width: size.width * 0.02,
+                            ),
+                            Text(
+                              data["sale"],
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Sale",
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500
+                              ),
+                            ),
+                            SizedBox(
+                              width: size.width * 0.02,
+                            ),
+                            Text(
+                              data["sale"],
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }else{
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
